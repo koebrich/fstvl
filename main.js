2cc95399b96804a1aff8eaea9c8bbd137364ed89
Katie Koebrich <katherine.r.koebrich@vanderbilt.edu>
2017-10-20 23:03:07 -0500

    did better

---
+++ main.js
@@ -0,0 +1,9 @@
+let http = require("http");
+
+http.createServer(function (request, response) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end('HI LILY!!!\n');
+}).listen(8080);
+
+// Console will print the message
+console.log('Server running at http://127.0.0.1:8080/');
